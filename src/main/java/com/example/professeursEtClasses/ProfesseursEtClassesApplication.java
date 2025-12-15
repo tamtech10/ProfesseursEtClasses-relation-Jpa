2b166e2869cb4c8d6deb5973ebf769268af9f7c1
tamtech10 <tamtech.dev@gmail.com>
2025-12-15 13:26:37 +0100

    Professeurs et Classes App Relations Jpa

---
+++ src/main/java/com/example/professeursEtClasses/ProfesseursEtClassesApplication.java
@@ -0,0 +1,13 @@
+package com.example.professeursEtClasses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfesseursEtClassesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfesseursEtClassesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/professeursEtClasses/ProfesseursEtClassesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.professeursEtClasses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfesseursEtClassesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
